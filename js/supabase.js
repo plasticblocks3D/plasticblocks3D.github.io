19a0edb8ae71a656b153c5dea8593090bf607e5b
plasticblocks3D <plasticblocks3D@outlook.com>
2026-07-15 12:20:32 -0400

    supabase addition begun

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://yalzogjuootvqanfggx.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_VRmhTrfIDWLfDYA53jwMPg_hU8twDsh";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
