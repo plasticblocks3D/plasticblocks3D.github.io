3721c826103a4e9d91a4b0f701c6eb6cf99ac13a
plasticblocks3D <plasticblocks3D@outlook.com>
2026-07-15 03:34:06 -0400

    Mobile Update and quote page fix

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('PlasticBlocks3D loaded');
+const menuButton = document.querySelector(".menu-toggle");
+
+const mobileMenu = document.querySelector(".mobile-menu");
+
+
+if(menuButton){
+
+    menuButton.addEventListener("click", () => {
+
+        mobileMenu.classList.toggle("active");
+
+        menuButton.classList.toggle("open");
+
+    });
+
+}
