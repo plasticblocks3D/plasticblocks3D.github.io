749f7109d526f9717fe71aed5cdb11cbc3f77fd4
plasticblocks3D <plasticblocks3D@outlook.com>
2026-07-15 15:27:29 -0400

    updated quote status

--- js/admin.js
+++ js/admin.js
@@ -78,7 +78,41 @@ ${quote.details}
 
 <p>
 <b>Status:</b>
-${quote.status}
+
+<select 
+class="status-select"
+data-id="${quote.id}">
+
+<option ${quote.status === "New" ? "selected" : ""}>
+New
+</option>
+
+<option ${quote.status === "Reviewing" ? "selected" : ""}>
+Reviewing
+</option>
+
+<option ${quote.status === "Quoted" ? "selected" : ""}>
+Quoted
+</option>
+
+<option ${quote.status === "Approved" ? "selected" : ""}>
+Approved
+</option>
+
+<option ${quote.status === "Printing" ? "selected" : ""}>
+Printing
+</option>
+
+<option ${quote.status === "Completed" ? "selected" : ""}>
+Completed
+</option>
+
+<option ${quote.status === "Archived" ? "selected" : ""}>
+Archived
+</option>
+
+</select>
+
 </p>
 
 
@@ -104,6 +138,66 @@ await supabaseClient.auth.signOut();
 
 
 window.location.href="login.html";
+
+
+});
+
+document
+.getElementById("logoutButton")
+.addEventListener("click", async ()=>{
+
+    await supabaseClient.auth.signOut();
+
+    window.location.href = "login.html";
+
+});
+
+document
+.addEventListener("change", async function(event){
+
+
+if(event.target.classList.contains("status-select")){
+
+
+const id = event.target.dataset.id;
+
+const newStatus = event.target.value;
+
+
+
+const { data, error } = await supabaseClient
+
+.from("quote_requests")
+
+.update({
+    status: newStatus
+})
+
+.eq("id", id)
+.select();
+
+
+console.log("UPDATED ROW:", data);
+console.log("UPDATE ERROR:", error);
+
+
+
+if(error){
+
+console.error(error);
+
+alert("Could not update status");
+
+}
+
+else{
+
+console.log("Status updated");
+
+}
+
+
+}
 
 
 });
